281da60b9164bed7aca707482de347b5201f1c0e
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2019-02-12 01:25:13 -0400

    Make the main thread sleep 17ms per loop to imitate 60Hz

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use std::time::Duration;
+use std::thread;
+
 use chip_8::Chip8;
+
+// Chip-8 Ran at 60 Hz, or 60 frames/second.
+// That means each frame lasted 16.7 =~ 17 ms.
+const TIME = Duration::from_millis(17);
 
 fn main() {
     let chip8 = Chip8::new();
 
     loop {
+        thread::sleep(TIME);
+
         chip8.cycle();
 
         chip8.draw();
